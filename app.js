ec8cf78e829da17ef62aaebfb0fa3327e66db1c1
BORAN08 <mahmutboran08@gmail.com>
2022-03-08 08:48:53 +0300

    toLocalString add

--- app.js
+++ app.js
@@ -5,11 +5,24 @@ let pm_am = document.querySelector(".pm-am");
 
 
 function digitalclock() {
-    
-hour.innerText=new Date().getHours().toString().padStart(2, "0");
-minute.innerText=new Date().getMinutes().toString().padStart(2, "0");
-second.innerText=new Date().getSeconds().toString().padStart(2, "0");}
+
 if (new Date().getHours()<12){
     pm_am.innerText="AM"
+    hour.innerText=new Date().getHours().toString().padStart(2, "0");
+}else if (new Date().getHours()>12){
+    pm_am.innerText="PM"
+    hour.innerText=new Date().getHours().toString().padStart(2, "0")-12;
 }
+
+minute.innerText=new Date().getMinutes().toString().padStart(2, "0");
+second.innerText=new Date().getSeconds().toString().padStart(2, "0");
+};
+
+
 setInterval(digitalclock,1000)
+
+
+//!https://www.w3schools.com/jsref/jsref_tolocalestring.asp
+// setInterval(() => {
+//     hour.innerText=new Date().toLocaleString('en-US', { hour: '2-digit', minute:"numeric",second:"numeric",hour12: true });
+// }, 1000);
